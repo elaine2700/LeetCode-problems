c7c0c1d749321b69cab285b130219949a2c61865
elaine2700 <easerrano.e@gmail.com>
2023-11-01 10:01:32 -0700

    Organizing files

--- Hard/956-TallestBillboard.py
+++ Hard/956-TallestBillboard.py
@@ -1,1 +1,8 @@
-print('Hello')
+class Solution:
+    def tallestBillboard(self, rods):
+        maxSum = sum(rods)
+        print(maxSum)
+    
+
+run_program = Solution()
+run_program.tallestBillboard([1,2,3,6])
